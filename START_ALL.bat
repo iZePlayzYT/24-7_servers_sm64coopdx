2e7b2b6bf93a84b1cb559c5d5f06dd4d6d0e23eb
iZePlayz <69536095+iZePlayzYT@users.noreply.github.com>
2024-08-27 04:59:19 +0200

    added batch scripts

---
+++ START_ALL.bat
@@ -0,0 +1,47 @@
+cd 1_vanilla
+START.bat
+cd..
+
+cd 2_star-road
+START.bat
+cd..
+
+cd 3_sm74
+START.bat
+cd..
+
+cd 4_sr7.5-coop-port
+START.bat
+cd..
+
+cd 5_dreamscape
+START.bat
+cd..
+
+cd 6_shining_stars
+START.bat
+cd..
+
+cd 7_sm74tya_sd
+START.bat
+cd..
+
+cd 8_star_revenge_1.5
+START.bat
+cd..
+
+cd 9_treasure_world
+START.bat
+cd..
+
+cd 10_mushroom_cup
+START.bat
+cd..
+
+cd 11_eureka
+START.bat
+cd..
+
+cd 12_galactic_journey
+START.bat
+cd..
